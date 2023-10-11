421187eb98eabe9921eda47ca6a8a5142a0da8fc
Thiago San Miguel <thiagosmperez1234@gmail.com>
2023-10-11 14:32:04 -0300

    Login

---
+++ src/main/java/ClaseLogicas/Usuario.java
@@ -0,0 +1,20 @@
+package ClaseLogicas;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private String nombreUusuario;
+    private String contraseñaUsuario;
+    private String rol;
+    
+    public String toCSV(){
+        return this.nombreUusuario+","+this.contraseñaUsuario+","+this.rol;              
+    }
+}
